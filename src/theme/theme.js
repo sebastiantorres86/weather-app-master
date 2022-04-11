aec9e49586fd700de6c3684e4cb0a9aa46ce4954
Sebastian Torres <seba.torres.dev@gmail.com>
2022-04-11 13:38:41 -0300

    create app skeleton (#1)

---
+++ src/theme/theme.js
@@ -0,0 +1,15 @@
+const theme = {
+  colors: {
+    lightGrey: '#E7E7EB',
+    white: '#fff',
+    blue: '#1E213A',
+    darkBlue: '#100E1D'
+  },
+  weight: {
+    thin: 100,
+    medium: 500,
+    semiBold: 600
+  }
+}
+
+export default theme
